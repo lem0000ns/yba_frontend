d92cedcba802650dabede180694adeec8e91e74d
lem0000ns <kevinyang.poke@gmail.com>
2024-10-19 12:05:31 -0700

    removed eslint warnings

--- eslint.config.js
+++ eslint.config.js
@@ -18,11 +18,8 @@ export default tseslint.config({
   },
   rules: {
     ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
     "@typescript-eslint/no-explicit-any": ["off"],
     "@typescript-eslint/no-unused-expressions": ["off"]
+    "react-refresh/only-export-components": ["off"]
   },
 })
